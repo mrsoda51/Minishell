9cba887676f4ab2b457f8bbdf71c23604cbc0268
SpaceMovie1992 <anahusic8@gmail.com>
2024-09-18 15:07:45 +0200

    still not done

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,43 +6,27 @@
 /*   By: ahusic <ahusic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 12:47:13 by ahusic            #+#    #+#             */
-/*   Updated: 2024/09/17 13:58:15 by ahusic           ###   ########.fr       */
+/*   Updated: 2024/09/18 14:28:43 by ahusic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	handle_error(t_error_type error_type, char *context)
-{
-	ft_putstr_fd("minishell: ", 2);
-	if (error_type == E_CD)
-	{
-		if (access(context, F_OK) == -1)
-		{
-			ft_putstr_fd("cd: ", 2);
-			ft_putstr_fd(context, 2);
-			ft_putstr_fd(": No such file or directory\n", 2);
-		}
-		else
-		{
-			ft_putstr_fd("cd: ", 2);
-			ft_putstr_fd(context, 2);
-			ft_putstr_fd(": No such file or directory\n", 2);
-		}
-	}
-	else
-		ft_putstr_fd("An error occurred\n", 2);
-}
-
 int	cd_path(char *old_path, char *new_path, t_minishell_data *mini_data)
 {
 	if (chdir(new_path) == -1)
 	{
-		handle_error(E_CD, new_path);
-		return EXIT_FAILURE;
+		if (access(new_path, F_OK) == -1)
+		{
+			builtin_error_msg(E_CD, "no such file or directory");
+			return (EXIT_FAILURE);
+		}
+		builtin_error_msg(E_CD, "not a directory");
+		return (EXIT_FAILURE);
 	}
-	//need to make an update environment functions
+	//need to make update environment functions
 	update_env_var("OLDPWD", old_path, mini_data);
 	update_env_var("PWD", new_path, mini_data);
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
+
